cc40a63f81d73a61e9671090afe2ec5ea331a1a8
Imai Raku <71510837+luck-tech@users.noreply.github.com>
2025-09-19 21:19:35 +0000

    middlewareにエラーレスポンスは任せる

--- api/src/routes/order.ts
+++ api/src/routes/order.ts
@@ -1,9 +1,9 @@
 import express from "express";
 import {validationResult} from "express-validator";
-import {UnauthorizedError, ValidationError} from "@/lib/errors";
+import {ValidationError, NotFoundError, BadRequestError} from "@/lib/errors";
 import {ensureAuthUser} from "@/middlewares/authentication";
 import {postOrder, getOrderById, getOrders} from "@/models/order";
-import {InsufficientStockError, OrderQueryParams} from "@/types/order";
+import {OrderQueryParams} from "@/types/order";
 import {createOrderValidations} from "@/middlewares/validation";
 
 export const router = express.Router();
@@ -12,7 +12,11 @@ router.post(
   "/",
   ensureAuthUser,
   createOrderValidations,
-  async (req: express.Request, res: express.Response) => {
+  async (
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction,
+  ) => {
     const result = validationResult(req);
     if (!result.isEmpty()) {
       throw new ValidationError(result.array().map(error => error.msg));
@@ -20,64 +24,43 @@ router.post(
     try {
       const params: OrderQueryParams[] = req.body;
       const orderResult = await postOrder(params, req.currentUser!.id);
+      if (!orderResult.success && orderResult.errors) {
+        throw new ValidationError(orderResult.errors);
+      }
       res.json(orderResult);
     } catch (error) {
-      if (error instanceof UnauthorizedError) {
-        res.status(401).json({message: "Unauthorized"});
-        return;
-      }
-      if (error instanceof InsufficientStockError) {
-        res
-          .status(422)
-          .json({message: "Validation Failed", errors: error.errors});
-        return;
-      }
-      console.error("Error occurred while creating orders:", error);
-      res.status(500).json({error: "Internal Server Error"});
+      next(error);
     }
   },
 );
 
-router.get("/:id", ensureAuthUser, async (req, res) => {
+router.get("/:id", ensureAuthUser, async (req, res, next) => {
   try {
     const orderId = parseInt(req.params.id, 10);
     if (isNaN(orderId)) {
-      res.status(400).json({message: "Invalid order ID"});
-      return;
+      throw new BadRequestError();
     }
 
     const order = await getOrderById(orderId, req.currentUser!.id);
     if (!order) {
-      res.status(404).json({message: "Not Found"});
-      return;
+      throw new NotFoundError();
     }
 
     res.json(order);
   } catch (error) {
-    if (error instanceof UnauthorizedError) {
-      res.status(401).json({message: "Unauthorized"});
-      return;
-    }
-    console.error("Error occurred while fetching order:", error);
-    res.status(500).json({error: "Internal Server Error"});
+    next(error);
   }
 });
 
-router.get("/", ensureAuthUser, async (req, res) => {
+router.get("/", ensureAuthUser, async (req, res, next) => {
   try {
     const orders = await getOrders(req.currentUser!.id); // ensureAuthUserで非nullは保証済み
     if (!orders) {
-      res.status(404).json({message: "Not Found"});
-      return;
+      throw new NotFoundError();
     }
 
     res.json(orders);
   } catch (error) {
-    if (error instanceof UnauthorizedError) {
-      res.status(401).json({message: "Unauthorized"});
-      return;
-    }
-    console.error("Error occurred while fetching orders:", error);
-    res.status(500).json({error: "Internal Server Error"});
+    next(error);
   }
 });
