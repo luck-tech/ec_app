c84becfb6baac29c6466f6c37cc65410d4feec21
Imai Raku <71510837+luck-tech@users.noreply.github.com>
2025-09-19 21:28:57 +0000

    productも同様にエラーレスポンスをミドルウェアに任せる

--- api/src/routes/product.ts
+++ api/src/routes/product.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import {validationResult} from "express-validator";
-import {UnauthorizedError, ValidationError} from "@/lib/errors";
+import {ValidationError} from "@/lib/errors";
 import {ensureAuthUser} from "@/middlewares/authentication";
 import {getProduct, getProductSearch} from "@/models/product";
 import {ProductQueryParams} from "@/types/product";
@@ -12,27 +12,35 @@ router.get(
   "/search",
   ensureAuthUser,
   getProductSearchValidation,
-  async (req: express.Request, res: express.Response) => {
-    const result = validationResult(req);
-    if (!result.isEmpty()) {
-      throw new ValidationError(result.array().map(error => error.msg));
+  async (
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction,
+  ) => {
+    try {
+      const result = validationResult(req);
+      if (!result.isEmpty()) {
+        throw new ValidationError(result.array().map(error => error.msg));
+      }
+
+      const params = req.query;
+      const filter = typeof params.filter === "string" ? params.filter : "";
+      const page =
+        typeof params.page === "string" ? parseInt(params.page, 10) : 1;
+      const results = await getProductSearch({
+        filter: filter,
+        page: page,
+        userId: req.currentUser!.id,
+      });
+
+      res.json(results);
+    } catch (error) {
+      next(error);
     }
-
-    const params = req.query;
-    const filter = typeof params.filter === "string" ? params.filter : "";
-    const page =
-      typeof params.page === "string" ? parseInt(params.page, 10) : 1;
-    const results = await getProductSearch({
-      filter: filter,
-      page: page,
-      userId: req.currentUser!.id,
-    });
-
-    res.json(results);
   },
 );
 
-router.get("/", ensureAuthUser, async (req, res) => {
+router.get("/", ensureAuthUser, async (req, res, next) => {
   try {
     const params: ProductQueryParams = req.query;
     const productIds = params.productIds;
@@ -47,11 +55,6 @@ router.get("/", ensureAuthUser, async (req, res) => {
 
     res.json(products);
   } catch (error) {
-    if (error instanceof UnauthorizedError) {
-      res.status(401).json({message: "Unauthorized"});
-      return;
-    }
-    console.error("Error occurred while searching products:", error);
-    res.status(500).json({error: "Internal Server Error"});
+    next(error);
   }
 });
