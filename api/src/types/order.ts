d395e1f20335d2c8e7fe9bec2b388569c6c01d72
Imai Raku <71510837+luck-tech@users.noreply.github.com>
2025-09-19 21:18:53 +0000

    不要なエラーを削除

--- api/src/types/order.ts
+++ api/src/types/order.ts
@@ -2,10 +2,3 @@ export type OrderQueryParams = {
   productId: number;
   quantity: number;
 };
-
-export class InsufficientStockError extends Error {
-  constructor(public errors: string[]) {
-    super("Insufficient stock");
-    this.name = "InsufficientStockError";
-  }
-}
